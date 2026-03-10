d09f89706e7ca97130ffbd731e4577945bcd503e
salilniem <salil.niem@bumail.net>
2026-03-10 16:36:03 +0700

    ball

--- Assets/Scripts/MagnusKick.cs
+++ Assets/Scripts/MagnusKick.cs
@@ -1,16 +1,39 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class MagnusKick : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float kickForce;
+    public float spinAmount;
+    public float magnusStreght = 0.5f;
+
+    Rigidbody rb;
+    bool isShot = false;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Keyboard.current.spaceKey.wasPressedThisFrame && !isShot)
+        {
+            rb.AddForce(Vector3.forward * kickForce, ForceMode.Impulse);
+            rb.AddTorque(Vector3.up * spinAmount);
+
+            isShot = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (!isShot) return;
+
+        Vector3 velocity = rb.linearVelocity;
+        Vector3 spin = rb.angularVelocity;
+        Vector3 magnusForce = magnusStreght * Vector3.Cross(spin, velocity);
+
+        rb.AddForce(magnusForce);
     }
 }
